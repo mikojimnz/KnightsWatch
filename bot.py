0a8b5b4a565b13e23527ead50a62098fa3e85f22
mikojimnz <mikojimnz@users.noreply.github.com>
2020-09-07 22:13:12 -0400

    Globalized praw, reload watchlist command

--- bot.py
+++ bot.py
@@ -24,6 +24,10 @@ from time import sleep
 
 CONST_REG = r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))'
 
+sub = None
+reddit = None
+watchlist = []
+
 def main():
     with open("settings.json") as jsonFile1:
         cfg = json.load(jsonFile1)
@@ -73,12 +77,12 @@ def main():
     os.system('cls' if os.name == 'nt' else 'clear')
 
     async def read_comments():
+        global reddit
+        global sub
+        global watchlist
         await client.wait_until_ready()
 
-        reddit = praw.Reddit(cfg['praw']['cred'])
-        sub = reddit.subreddit(cfg['praw']['sub'])
         commentStream = sub.stream.comments(skip_existing=cfg['praw']['skipExisting'], pause_after=-1)
-        watchlist = []
 
         if (cfg['praw']['toolbox']['monitorUsers']):
             usernotesJson = json.loads(sub.wiki[cfg['praw']['toolbox']['usernotePage']].content_md)
@@ -156,12 +160,38 @@ def main():
 
     @client.event
     async def on_ready():
+        global reddit
+        global sub
+        reddit = praw.Reddit(cfg['praw']['cred'])
+        sub = reddit.subreddit(cfg['praw']['sub'])
+
         cprint(f'    Discord connection established, logged in as {client.user}', 'green')
         client.loop.create_task(read_comments())
 
     @client.command()
     async def ping(ctx):
         await ctx.message.channel.send("Pong!")
+
+    @client.command()
+    async def reload(ctx, *args):
+        global reddit
+        global sub
+        global watchlist
+
+        if not args:
+            await ctx.send('No argument found')
+        elif ((args[0] == 'watchlist') and (cfg['praw']['toolbox']['monitorUsers'])):
+            watchlist = []
+            usernotesJson = json.loads(sub.wiki[cfg['praw']['toolbox']['usernotePage']].content_md)
+            decompressed = zlib.decompress(base64.b64decode(usernotesJson['blob']))
+            for user in json.loads(decompressed).keys():
+                 watchlist.append(user)
+
+            print(f'{len(watchlist)} users in toolbox usernotes')
+            await ctx.send(f'Watchlist reloaded with {len(watchlist)} users in toolbox usernotes')
+        else:
+            await ctx.send(f'Invalid argument {args}')
+
 
     @client.event
     async def on_reaction_add(reaction, user):
@@ -198,7 +228,7 @@ def main():
             json.dump(tmp, jsonFile2)
             jsonFile2.truncate()
 
-        await reaction.message.channel.send(f'Comment added to training data:\n```{inp[:25]}```')
+        await reaction.message.channel.send(f'Comment added to training data: `{inp[:25]}`')
         print(f'{reaction.emoji}: {inp}')
 
     client.run(cfg['discord']['clientID'])
